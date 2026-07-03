c44b4df8dbd34dc39b68cdaf0b142b698422e057
Juan JoseCb <jm6619940@gmail.com>
2026-07-03 14:05:30 -0400

    Bust cached module imports

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-import { renderLoginShell, renderAppShell } from "./components/layout.js";
-import { openModal } from "./components/modal.js";
-import { toast } from "./components/toast.js";
-import { dataService } from "./services/dataService.js";
-import { renderDashboard } from "./pages/dashboard.js";
-import { renderNewRequest } from "./pages/newRequest.js";
-import { renderRequestsTable } from "./pages/requestsTable.js";
-import { renderUsers } from "./pages/users.js";
-import { renderCatalogs } from "./pages/catalogs.js";
-import { renderProfile } from "./pages/profile.js";
-import { renderNotifications } from "./pages/notifications.js";
+import { renderLoginShell, renderAppShell } from "./components/layout.js?v=20260703-8";
+import { openModal } from "./components/modal.js?v=20260703-8";
+import { toast } from "./components/toast.js?v=20260703-8";
+import { dataService } from "./services/dataService.js?v=20260703-8";
+import { renderDashboard } from "./pages/dashboard.js?v=20260703-8";
+import { renderNewRequest } from "./pages/newRequest.js?v=20260703-8";
+import { renderRequestsTable } from "./pages/requestsTable.js?v=20260703-8";
+import { renderUsers } from "./pages/users.js?v=20260703-8";
+import { renderCatalogs } from "./pages/catalogs.js?v=20260703-8";
+import { renderProfile } from "./pages/profile.js?v=20260703-8";
+import { renderNotifications } from "./pages/notifications.js?v=20260703-8";
 
 const root = document.querySelector("#app");
 const state = {
