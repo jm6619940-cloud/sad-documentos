1fbdae566cba9ff26170ae1e14a1f0153b59e961
Juan JoseCb <jm6619940@gmail.com>
2026-07-14 17:27:05 -0400

    Refina tabla de solicitudes

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "sad-static-v20260714-16";
+const CACHE_NAME = "sad-static-v20260714-17";
 const NOTIFICATION_ICON = new URL("./assets/icon-192.png?v=20260710-1", self.registration.scope).href;
 const STATIC_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest?v=20260710-2",
-  "./css/styles.css?v=20260714-16",
-  "./js/app.js?v=20260714-16",
+  "./css/styles.css?v=20260714-17",
+  "./js/app.js?v=20260714-17",
   "./js/components/layout.js?v=20260708-14",
   "./js/components/modal.js?v=20260708-12",
   "./js/components/toast.js?v=20260708-12",
